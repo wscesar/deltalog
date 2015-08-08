08a7651ed20b29db02fb9ad35fc23d597ccb6f37
wscesar <wscesar@gmail.com>
2015-08-07 22:38:36 -0300

    introduzido jade

--- config.rb
+++ config.rb
@@ -1,6 +1,9 @@
 http_path = ""
 css_dir = "assets/css"
 sass_dir = "assets/sass"
+haml_dir = "assets/haml"
+jade_dir = "assets/jade"
+html_dir = "pages/html"
 images_dir = "assets/img"
 javascripts_dir = "assets/js"
 output_style = :compressed
